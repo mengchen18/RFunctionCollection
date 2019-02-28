f1c86d54821919bc60d0f761b727ec93d6bbf79e
mengchen18 <mengchen18@gmail.com>
2019-02-28 10:52:03 +0100

    Update basicMQC.R
    x could be a regular matrix now

--- basicMQC.R
+++ basicMQC.R
@@ -3,9 +3,10 @@
 #' @param x the input matrix, usually a proteingroups table from maxquant
 #' @param cols the columns to be included in the QC, usually the intensity, iBAQ or LFQ columns
 #' @param pheno the pheno type vector, should be the same length as cols
+#' @param xmpq whether x is an maxquant protein group input
 #' @import matrixStats
 #' @import randomcoloR
-basicMQC <- function(x, cols, pheno) {
+basicMQC <- function(x, cols, pheno, xmpg = TRUE) {
   
   require(matrixStats)
   require(randomcoloR)
@@ -14,20 +15,21 @@ basicMQC <- function(x, cols, pheno) {
   pheno <- pheno[ord]
   cols <- cols[ord]
   
-  emat <- x[, cols]
-  colnames(emat) <- gsub("LFQ.intensity.|iBAQ.|Intensity.", "", colnames(emat))
-  
   pheno <- as.factor(pheno)
   n <- nlevels(pheno)
   pal <- structure(distinctColorPalette(n), names = levels(pheno))
   
-  i1 <- grepl("^CON", x$Majority.protein.IDs)
-  i2 <- grepl("^REV", x$Majority.protein.IDs)
-  i3 <- x$Only.identified.by.site == "+"
-  i4 <- rowSums(emat, na.rm = TRUE) == 0
-  i <- !(i1 | i2 | i3 | i4)
-  
-  emat <- apply(emat[i, ], 2, as.numeric)
+  if (xmpg) {
+      i1 <- grepl("^CON", x$Majority.protein.IDs)
+      i2 <- grepl("^REV", x$Majority.protein.IDs)
+      i3 <- x$Only.identified.by.site == "+"
+      i4 <- rowSums(emat, na.rm = TRUE) == 0
+      i <- !(i1 | i2 | i3 | i4)
+      emat <- x[, cols]
+      colnames(emat) <- gsub("LFQ.intensity.|iBAQ.|Intensity.", "", colnames(emat))
+      emat <- apply(emat[i, ], 2, as.numeric)
+  } else
+    emat <- x
   emat[is.na(emat)] <- 0
   
   # id plot
