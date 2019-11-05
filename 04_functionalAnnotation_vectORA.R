238e5887d9ff8154e846202381a69fcc89ce555b
mengchen18 <mengchen18@gmail.com>
2019-11-05 21:28:05 +0100

    Update 04_functionalAnnotation_vectORA.R

--- 04_functionalAnnotation_vectORA.R
+++ 04_functionalAnnotation_vectORA.R
@@ -1,48 +1,49 @@
 #' three columns in 
-#' @param gene.id the gene id, should be the same length as p.value. It should be unique and not delimited for multiple genes.
-#' @param p.val the p.val from a test, usually a test for differential expression
-#' @param geneset a character vector of indicates which geneset are associated with a specific gene.id, should be delimited by semicolon (;).
-#'   It should have the same length as p.value
-#' @param p.thresh the threshold of p value, lower than this value would be considered as significant
-#' @param min.size the minimum size of geneset that should be considered
-#' 
+#' @param pathways List of gene sets to check.
+#' @param genelist A list of gene/proteins you want to annotate, often from differential expression analysis.
+#'  The ID needs to be the same as in pathways
+#' @param background An integer to indicate the size of background; or a character vector 
+#'  stores all background genes, the ID needs to be the same as in pathways
+#' @param trimPathway remove ID from pathways that are not present in the background, 
+#'  only used if background is a gene/protein name list.
+#' @param minOverlap the minimum required overlap between pathway and gene list, if the overlap is lower
+#'  than this value, no test would be done on this pathway
+#' @param minSize the minimum size of pathway should be tested
+#' @param minSize the maximum size of pathways should be tested
+#' @param unconditional.or calculate odds ratio using Maximum Likelihood Estimate (the sample odds ratio). 
+#'  Note that the conditional Maximum Likelihood Estimate (MLE) is used in fisher.test. 
+#' @param mtc.method multiple test correction methods, passed to p.adjust function
 #' @import fastmatch
+require(fastmatch)
 
-
-library(fastmatch)
-
-vectORA <- function(pathways, pvec, pvec.cut, pvecAnnot=NULL, 
-                    minOverlap = 3, minSize=5, maxSize=Inf, size_background = NULL,
+vectORA <- function(pathways, genelist, background, trimPathway = FALSE,
+                    minOverlap = 3, minSize=5, maxSize=Inf, 
                     unconditional.or = TRUE, mtc.method = "fdr") {
+  # check id, duplicates
+  genelist <- unique(genelist)
+  if (length(background) == 1 && is.integer(background)) {
+  	bkgn <- background 
+  } else if (length(background) > 1 && is.character(background)) {
+  	background <- unique(background)
+  	bkgn <- length(background)
+  	if (trimPathway)
+  		pathways <- lapply(pathways, function(x) x[x %fin% background]) 
+  } else 
+  	stop("Unknown background type!")
   
-  if (is.null(pvecAnnot))
-    pvecAnnot <- names(pvec)
-  
-  allid <- unique(unlist(pvecAnnot))
-  allid.pathways <- unique(unlist(pathways))
-  bkgn <- sum(allid %fin% allid.pathways)
-  if (!is.null(size_background)) {
-    bkgn <- max(size_background, bkgn)
-    if (size_background < bkgn)
-      message(paste0("size_background is set to ", bkgn, "!" ))
-  }
-  
-  deid <- unique(unlist(pvecAnnot[pvec < pvec.cut]))
-  overlap <- lapply(pathways, function(x) x[x %fin% deid])
+  overlap <- lapply(pathways, function(x) x[x %fin% genelist])
   
   nol <- sapply(overlap, length)
   ngs <- sapply(pathways, length)
   i <- nol >=  minOverlap & ngs >= minSize & ngs <= maxSize
-  
   pathways <- pathways[i]
   overlap <- overlap[i]
   nol <- nol[i]
   ngs <- ngs[i]
   
-  
   bdf <- vectORA.core(
     n.overlap = nol, 
-    n.de = length(deid), 
+    n.de = length(genelist), 
     n.gs = ngs, 
     n.bkg = bkgn, 
     unconditional.or = unconditional.or, mtc.method = mtc.method)
